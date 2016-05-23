7bdbe704ebe4efa1b3b8b0b86657a29c0a2ba8a5
riccardoschuller <cartuciolo@hotmail.it>
2016-05-23 19:48:32 +0200

    primo

---
+++ RistoranteBio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RistoranteBio
+//
+//  Created by riccardo on 19/05/16.
+//  Copyright © 2016 riccardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RistoranteBio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RistoranteBio
+//
+//  Created by riccardo on 19/05/16.
+//  Copyright © 2016 riccardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
